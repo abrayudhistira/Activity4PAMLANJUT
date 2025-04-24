76f59f7d8363ceebb524f6653945fd311146183a
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-25 02:32:50 +0700

    Fixed Home Page

--- lib/presentation/homePage.dart
+++ lib/presentation/homePage.dart
@@ -10,8 +10,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Selamat Datang di Home Page'),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const OrderPage()),
+              );
+            }, child: Text('Order Now')),
+          ],
+        ),
+      ),
     );
   }
 }
