5b406d8a69148d9415559f79c70387ab2cfd9659
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-25 02:26:45 +0700

    Fixed Login

--- lib/presentation/login.dart
+++ lib/presentation/login.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-  const Login({ Key? key }) : super(key: key);
+  const Login({super.key});
 
   @override
   _LoginState createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(context,
+                     MaterialPageRoute(builder: (context) => const Homepage()));
+                  }
+                },
+                child: const Text('Login'),
+              ),
+              TextButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterPage()));
+              }, 
+              child: const Text('Belum Punya akun? Daftar'),
+              )
+            ],
+          ),
+          )
+        ),
     );
   }
 }
